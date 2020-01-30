ef93fb202f232845089494222a34cc1d3e3ee54d
PRANAV-PANU <pranav.juit@gmail.com>
2020-01-30 23:52:47 +0530

    create database instance in main activity

--- app/src/main/java/com/example/sqlitedb/MainActivity.java
+++ app/src/main/java/com/example/sqlitedb/MainActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseHelper mydb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mydb = new DatabaseHelper(this);
+        mydb.getWritableDatabase();
     }
 }
